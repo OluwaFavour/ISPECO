80d8267cc05725e8110920090f50c09b837fde1f
Favour Olusayo <fsticks8187@gmail.com>
2024-05-28 12:55:09 +0100

    feat: Add .dockerignore file and update .gitignore
    This commit adds a .dockerignore file to the repository and updates the .gitignore file. The .dockerignore file specifies the files and directories that should not be copied to the Docker container during the build process. The .gitignore file now includes the newly added .dockerignore file.
    
    These changes improve the Docker build process by excluding unnecessary files and directories from being included in the container, reducing its size and improving performance.

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Read secrets from files and export them as environment variables
+export SECRET_KEY=$(cat /run/secrets/secret_key)
+export DATABASE_URL=$(cat /run/secrets/database_url)
+export EMAIL_HOST_PASSWORD=$(cat /run/secrets/email_host_password)
+export FERNET_KEY=$(cat /run/secrets/fernet_key)
+export SUPERUSER_PASSWORD=$(cat /run/secrets/superuser_password)
+
+# Handle migrations and static files
+python manage.py collectstatic --noinput
+python manage.py makemigrations user_authentication
+python manage.py migrate
+python manage.py makemigrations camera_integration live_streaming
+python manage.py migrate
+python manage.py create_superuser
+
+# Execute the command passed as arguments to this script
+exec "$@"
